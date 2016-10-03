6f47c8a2c3d44034b9ce493281e65445cd69e061
speeeee <snsallay@gmail.com>
2016-10-02 23:36:00 -0400

    change Function so that s-expr head is evaluated

--- arrcc.hs
+++ arrcc.hs
@@ -3,6 +3,9 @@
 
 import Debug.Trace
 
+-- (lambda (type Int32 Int32) (quote + 1 (head :A:))) 
+--   --> Function "Lambda" (+ 1 (head :A:)) [TInt32,TInt32]
+
 data SType    = TInt8 | TInt16 | TInt32 | TInt64 | TFloat32 | TFloat64
               | Symbol | Quot | TArr | TList | Func [SType] | Any | LError deriving (Show)
 -- because of Any, any function type must be on RHS.
@@ -15,7 +18,7 @@ instance Show Function where
 instance Eq Function where
   _ == _ = False
 
-data SExp = SApp Function [SExp] 
+data SExp = SApp SExp [SExp] | SFunc Function
           | SL [Char] SType | Quote SExp | SArr [SType] | SList [SExp]
           | Arg | SError [Char]  deriving (Show,Eq)
 
@@ -29,11 +32,13 @@ fs = [Function "+" (\nfs lst -> case lst of
      ,Function "def" (\nfs lst -> case lst of
                        (SL n _:SArr a:Quote b:_) ->
                          (Function n (\nnfs nlst -> eval nnfs $ repl nlst b) [] a:nfs,SList [])
-                       _ -> (nfs,SError "Error.\n")) [] [Symbol,TArr,Quot,TList]]
+                       _ -> (nfs,SError "Error.\n")) [] [Symbol,TArr,Quot,TList]
+     ,Function "error" (\_ _ -> ([],SError "Error.\n")) [] []]
 add, def :: Function
-add = head fs
-def = fs!!2
-shead = fs!!1
+add    = head fs
+def    = fs!!2
+shead  = fs!!1
+serror = fs!!3
 
 repl :: [SExp] -> SExp -> SExp
 repl l k = case k of Arg           -> SList l
@@ -46,20 +51,26 @@ ltype (SL _ t) = t
 ltype (Quote _) = Quot
 ltype (SArr _) = TArr
 ltype (SList _) = TList
+ltype (SFunc (Function _ _ _ io)) = Func io
 ltype _ = LError
 
 appF :: Function -> ([Function],[SExp]) -> ([Function],SExp)
-appF (Function n f _ t ) (nfs,e) =
+appF (Function "serror" _ _ _) _ = ([], SError "Error: s-exp head not a function.\n")
+appF (Function n f _ t) (nfs,e) =
   if map ltype e /= (init t) then
     (nfs,SError $ concat ["Error: type mismatch: ", show $ map ltype e, " ", show $ init t, "\n"])
   else (f nfs) e
 
 eval :: [Function] -> SExp -> ([Function],SExp)
-eval nfs (SApp f a) = appF f $ (fst $ last ev, map snd ev) where ev = map (eval nfs) a
+eval nfs (SApp f a) = appF fun $ (fst $ last ev, map snd ev)
+  where ev = map (eval nfs) a
+        fun = case snd $ eval nfs f of (SFunc fu) -> fu
+                                       _          -> serror
 eval nfs (SList a) = (fst $ last ev, SList $ map snd ev) where ev = map (eval nfs) a
 eval nfs a = (nfs,a)
 
 main = do
-  let (nfs,res) = eval [] (SApp def [SL "inc" Symbol, SArr [TInt32,TInt32]
-                                   ,Quote (SApp add [SApp shead [Arg],SL "1" TInt32])])
-  putStrLn $ show $ eval [] (SApp (nfs!!0) [SL "1" TInt32]) 
+  let (nfs,res) = eval [] (SApp (SFunc def) [SL "inc" Symbol, SArr [TInt32,TInt32]
+                                            ,Quote (SApp (SFunc add)
+                                               [SApp (SFunc shead) [Arg],SL "1" TInt32])])
+  putStrLn $ show $ eval [] (SApp (SFunc $ nfs!!0) [SL "1" TInt32]) 
